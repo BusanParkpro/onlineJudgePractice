ecfb2b8c43997b9d5232c18782554bbfd7424de6
DongJaePark <103563454+BusanParkpro@users.noreply.github.com>
2022-06-16 20:44:22 +0900

    [Silver V] Title: 소인수분해, Time: 160 ms, Memory: 14288 KB -BaekjoonHub

---
+++ 백준/Silver/11653. 소인수분해/소인수분해.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        while (N == 1)
+            break;
+
+        for (int i = 2; i <= N; i++) {
+            if(N % i == 0) {
+                N /= i;
+                System.out.println(i);
+                i = 1;
+            }
+        }
+    }
+}
